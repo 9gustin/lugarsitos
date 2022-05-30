ac0e2dc7628d2d9057ed4fde793cba96487b0519
Agustin Vazquez <agustinvazquez453@gmail.com>
2022-05-30 18:18:45 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# rnd pa
+
+## pending integrations
+
+Make it PWA
+
+Native
+ - camera
+ - camera for qr code
+ - camera to text
+ - voice to text
+
+Firebase
+ - dynamic links? maybe para que sirve??
+ - remote config para que sirve??
+
+
+## todo
+generate manifest and favicon
+https://www.simicart.com/manifest-generator.html/
+https://realfavicongenerator.net/
+
+Generate a QR reader
---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+import React, { useState, useEffect } from 'react';
+import { QrReader } from 'react-qr-reader';
+
+const Test = (props) => {
+  const [data, setData] = useState('No result');
+
+  useEffect(() => alert(data), [data])
+
+  return (
+    <>
+      <QrReader
+        onResult={(result, error) => {
+          if (!!result) {
+            setData(result?.text);
+          }
+
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        style={{ width: '100%' }}
+      />
+      <p>{data}</p>
+    </>
+  );
+};
+
+export default function Home() {
+  return <Test />
+}
