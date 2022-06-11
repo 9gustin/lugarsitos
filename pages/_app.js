87b1b8058c2b8c8823ccaf97d81c71dc9c75b8cf
Agustin Vazquez <agustinvazquez453@gmail.com>
2022-06-11 00:39:55 -0300

    update name

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }) {
       <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
       <meta name='description' content='Description' />
       <meta name='keywords' content='Keywords' />
-      <title>Next.js PWA Example</title>
+      <title>lugarsitos</title>
 
       <link rel="manifest" href="/manifest.json" />
       <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
